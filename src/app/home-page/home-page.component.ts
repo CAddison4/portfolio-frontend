b5e1a5b550be43bc910eb77b58b4afe40b918170
Catherine Addison <caddison4@my.bcit.ca>
2023-03-19 14:10:37 -0700

    final project

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Project } from '../models/project';
+import { ProjectService } from '../project.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnInit {
+  constructor(
+    private projectService: ProjectService,
+    private route: ActivatedRoute
+  ) {}
 
+  projects: Project[] = [];
+  getProjects(): void {
+    // this.projects = this.projectService.getProjects();
+    this.projectService
+      .getProjects()
+      .subscribe((projects) => (this.projects = projects));
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      const segment: string = this.route.snapshot.url[1]?.path;
+      console.log('test', this.route.snapshot.url);
+      console.log('segment', segment);
+
+      this.getProjects();
+    });
+  }
 }
